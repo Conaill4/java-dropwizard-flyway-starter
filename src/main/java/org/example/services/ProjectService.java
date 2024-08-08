11a390172346a3057176459f06610ade51fbb806
Megan <meganc04@hotmail.co.uk>
2024-08-08 16:44:19 +0100

    creating ProjectDao etc again

--- src/main/java/org/example/services/ProjectService.java
+++ src/main/java/org/example/services/ProjectService.java
@@ -1,25 +1,16 @@
 package org.example.services;
 
 import org.example.daos.ProjectDao;
-import org.example.models.Project;
 import org.example.models.ProjectRequest;
 
 import java.sql.SQLException;
-import java.util.List;
+
 
 public class ProjectService {
     ProjectDao projectDao;
 
     public ProjectService(final ProjectDao projectDao) {
         this.projectDao = projectDao;
-    }
-
-    public List<Project> getAllProjects() throws SQLException {
-        // throw new SQLException();
-        //return ProjectMapper.mapOrderListToOrderResponseList(
-                //projectDao.getAllProjects();
-        // return orderDao.getAllOrders();
-        return projectDao.getAllProjects();
     }
 
     public int createProject(final ProjectRequest projectrequest)
@@ -32,15 +23,4 @@ public class ProjectService {
         }
         return id;
     }
-
-    public void updateProduct(final int id, final ProjectRequest projectRequest)
-            throws  SQLException {
-        Project projectToUpdate = projectDao.getProjectById(id);
-        if (projectToUpdate == null) {
-            throw new SQLException();
-        }
-        projectDao.updateProject(id, projectRequest);
-
-    }
-
 }
