97366731d6e7c5271a01531d0ee53664ad3c5b96
de-kainos <daniel.edwards@kainos.com>
2024-08-08 16:33:08 +0100

    DeliveryEmployee changes

--- src/main/java/org/example/daos/DeliveryEmployeeDao.java
+++ src/main/java/org/example/daos/DeliveryEmployeeDao.java
@@ -11,24 +11,5 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DeliveryEmployeeDao {
-    public List<DeliveryEmployee> getAllDeliveryEmployees() throws SQLException {
-        List<DeliveryEmployee> deliveryEmployees = new ArrayList<>();
 
-        try(Connection connection = DatabaseConnector.getConnection()) {
-            Statement statement = connection.createStatement();
-
-            ResultSet resultSet = statement.executeQuery(
-                    "SELECT DeliveryID, EmployeeID FROM `DeliveryEmployee` join `Employee` using (EmployeeID);");
-
-            while (resultSet.next()) {
-                DeliveryEmployee deliveryEmployee = new DeliveryEmployee(
-                        resultSet.getInt("DeliveryID"),
-                        new Employee(resultSet.getInt("EmployeeID"));
-
-                deliveryEmployees.add(deliveryEmployee);
-            }
-        }
-
-        return deliveryEmployees;
-    }
 }
