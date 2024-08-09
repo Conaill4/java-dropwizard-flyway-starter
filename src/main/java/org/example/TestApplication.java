3b89545b4a204adc2dadc5e873a92adbf4e54790
Conaill O Neill <85241039+Conaill4@users.noreply.github.com>
2024-08-09 01:42:39 -0700

    Update TestApplication.java

--- src/main/java/org/example/TestApplication.java
+++ src/main/java/org/example/TestApplication.java
@@ -39,10 +39,10 @@ public class TestApplication extends Application<TestConfiguration> {
     public void run(final TestConfiguration configuration,
                     final Environment environment) {
         environment.jersey()
+            .register(new ClientController(new ClientService(new ClientDao())));
 
-       .register(new ClientController(new ClientService(new ClientDao())));
-
-        .register(new EmployeeController(new EmployeeService(new EmployeeDao()))
+        environment.jersey()
+            .register(new EmployeeController(new EmployeeService(new EmployeeDao()));
 
     }
 
